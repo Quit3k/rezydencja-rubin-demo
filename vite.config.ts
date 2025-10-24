50bef2886d5c2031bf04743827803fc53df23810
Eryk <erykkokoczkaa@gmail.com>
2025-10-24 12:23:28 +0200

    Updates

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' // Importujemy nową wtyczkę
-
-export default defineConfig({
-  // Dodajemy wtyczkę tailwindcss() do tablicy plugins
-  plugins: [react(), tailwindcss()],
-  base: '/rezydencja-rubin-demo/',
-})
